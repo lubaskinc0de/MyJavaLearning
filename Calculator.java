9165f3a2b1a246e2e5389e0a8cb0dbd3a1fb2968
lubaskinc0de <lubaskincorporation@gmail.com>
2022-12-23 23:01:36 +0300

    Yet another java calculator!

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,84 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Calculator {
     public static void main(String[] args) {
-
-    }
-
-    public static <T> T getInput(String text, T inputType) {
         final Scanner in = new Scanner(System.in);
 
-        System.out.print(text);
+        selectAction(in);
+        closeIn(in);
+    }
 
+    public static <T> T getInput(Scanner in, String text,
+                                 Function<Scanner, T> getResult) {
+
+        System.out.println(text);
+
+        return getResult.apply(in);
+    }
+
+    public static void closeIn(Scanner in) {
         in.close();
+    }
+
+    public static void selectAction(Scanner scanner) {
+        final Map<String, BiFunction<Integer, Integer, Float>> actions =
+                new HashMap<>() {{
+                    put("+", Calculator::sum);
+                    put("-", Calculator::sub);
+                    put("*", Calculator::mul);
+                    put("/", Calculator::div);
+                }};
+
+        final String action = getInput(scanner, """
+                Добро пожаловать!
+                Это самый простейший калькулятор
+                                
+                Выберите желаемую операцию с числами:
+                                
+                (+) - Сложение
+                (-) - Вычитание
+                (*) - Умножение
+                (/) - Деление
+                       
+                """, Scanner::nextLine
+        );
+
+        if (!(String.join("", actions.keySet()).contains(action))) {
+            throw new ArithmeticException("Вы должны выбрать корректное " +
+                    "действие");
+        }
+
+        final int numberOne = getInput(scanner, "Введите первое число: ",
+                Scanner::nextInt);
+
+        final int numberTwo = getInput(scanner, "Введите второе число: ",
+                Scanner::nextInt);
+
+        final BiFunction<Integer, Integer, Float> actionCallback =
+                actions.get(action);
+
+        final Float result = actionCallback.apply(numberOne, numberTwo);
+
+        System.out.printf("Результат: %.2f", result);
+    }
+
+    public static float sum(int numberOne, int numberTwo) {
+        return (float) (numberOne + numberTwo);
+    }
+
+    public static float sub(int numberOne, int numberTwo) {
+        return (float) (numberOne - numberTwo);
+    }
+
+    public static float div(int numberOne, int numberTwo) {
+        return (float) (numberOne / numberTwo);
+    }
+
+    public static float mul(int numberOne, int numberTwo) {
+        return (float) (numberOne * numberTwo);
     }
 }
