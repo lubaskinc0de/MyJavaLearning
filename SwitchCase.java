51c2af62db89b713269ed175252733c5bb9fb6d5
lubaskinc0de <lubaskincorporation@gmail.com>
2022-12-24 21:40:08 +0300

    switch case

---
+++ SwitchCase.java
@@ -0,0 +1,18 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int condition = 3;
+
+        switch (condition) {
+            case 1:
+                System.out.println("condition=1");
+                // break что бы дальше не шла проверка на другие case
+                break;
+            case 2:
+                System.out.println("condition=2");
+                break;
+            case 3:
+                System.out.println("condition=3");
+                break;
+        }
+    }
+}
