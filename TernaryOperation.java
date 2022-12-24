74195feef913191cb6968c55547bd5eb9c6e067f
lubaskinc0de <lubaskincorporation@gmail.com>
2022-12-24 21:44:14 +0300

    ternary operation

---
+++ TernaryOperation.java
@@ -0,0 +1,8 @@
+public class TernaryOperation {
+    public static void main(String[] args) {
+        int ternary = 5 == 2 + 3 ? 4 : 3; // если 5 == 2 то результат
+        // выражения 4 else 3
+
+        System.out.println(ternary);
+    }
+}
